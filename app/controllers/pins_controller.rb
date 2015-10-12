e3edbd7ec6a47032df3d08aa53e827a5468398bf
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2015-10-12 13:51:27 -0400

    Created users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,28 +7,46 @@ class PinsController < ApplicationController
   def show
     @pin = Pin.find(params[:id])
   end
-  
+
   def show_by_name
   	@pin = Pin.find_by_slug(params[:slug])
   	render :show
   end
-  
+
   def new
     @pin = Pin.new
   end
-  
-  def create
-  	@pin = Pin.create(pin_params)
-  	@pin.save
-  end
-  
-  private
-  	def pin_params
-  		params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image)
-  end
-  
+
   def edit
-  @pin = Pin.find(params[:id])
+    @pin = Pin.find(params[:id])
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+    if @pin.update_attributes(pin_params)
+      redirect_to pin_path(@pin)
+    else
+      @error = @pin.errors
+      render :edit
+    end
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @error = @pin.errors
+      render :new
+    end
+  end
+
+
+private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image)
   end
   
 end
